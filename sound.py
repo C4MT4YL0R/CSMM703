2320300aad50dbefe14422216158160957b701a6
Cameron Taylor <taylor_cam@Camerons-MacBook-Pro.local>
2014-12-02 11:27:19 -0500

    sound file update

--- sound.py
+++ sound.py
@@ -1,10 +1,11 @@
 import OSC
 
-def test():
+def test():	
+    print 'This is running'
     c = OSC.OSCClient()
     c.connect(('127.0.0.1', 57120))
     oscmsg = OSC.OSCMessage()
     oscmsg.setAddress("/startup")   
-    #oscmsg.append('HELLO') 
+    
     c.send(oscmsg)
     
