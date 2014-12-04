977946c2a79b4b079029d7489ab50d7636983c11
Sadia "Rain" Sharmin <rain.sharmin@gmail.com>
2014-12-03 21:18:10 -0500

    moon hits star and gen art gets created

--- moveAndCollide.py
+++ moveAndCollide.py
@@ -1,4 +1,6 @@
 import bge
+import Generative
+import random
 
 def test():
 	moveObject('Moon');
@@ -11,12 +13,11 @@ def moveObject(objName):
 	cury=obj.position.y;
 	curz=obj.position.z;
 	obj.worldPosition = [curx+obj['dx'],cury+obj['dy'],curz+obj['dz']];
-		
+				
 def directObject(objName):
 	scene = bge.logic.getCurrentScene();
 	obj = scene.objects[objName];
 	sensor = obj.sensors['c']; # get the collision sensor
-	
 	hitObject = sensor.hitObject; # get the object we hit
 	
 	if hitObject: # note: we check if this exists, because...
@@ -24,27 +25,31 @@ def directObject(objName):
 		hitName = hitObject.name; # get the name of the object we hit
 		print("hit!");
 		print("hitName: " + str(hitName));
-		if hitName=="UpArrow":
-			obj['dz'] = 0.1;
+		Generative.makeGenerativeArt();
+		chooseRandomDirection('Moon',random.randint(0,5));
+	
+def chooseRandomDirection(obj,n):
+		if n==0:
+			obj['dz'] = "0.1";
 			obj['dy'] = 0;
 			obj['dx'] = 0;
-		if hitName=="DownArrow":
+		elif n==1:
 			obj['dz'] = -0.1
 			obj['dy'] = 0
 			obj['dx'] = 0
-		if hitName=="LeftXArrow":
+		elif n==2:
 			obj['dz'] = 0
 			obj['dy'] = 0
 			obj['dx'] = 0.1
-		if hitName=="RightXArrow":
+		elif n==3:
 			obj['dz'] = 0
 			obj['dy'] = 0
 			obj['dx'] = -0.1
-		if hitName=="LeftYArrow":
+		elif n==4:
 			obj['dz'] = 0
 			obj['dy'] = 0.1
 			obj['dx'] = 0
-		if hitName=="RightYArrow":
+		else:
 			obj['dz'] = 0
 			obj['dy'] = -0.1
 			obj['dx'] = 0
